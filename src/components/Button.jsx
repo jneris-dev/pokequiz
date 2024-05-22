c645ef87126bc8dec98f16fc775578d1bce231b5
jneris-dev <site@jneris.com.br>
2024-05-21 21:44:27 -0300

    Add style focus button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,9 +4,9 @@ export function BtnSelect({status, a, verify, history, pokemon, i, playVoice, ha
     const button = classNames(
         "cursor-pointer w-full rounded-full border-2 py-3 text-white font-bold text-lg flex items-center justify-center hover:bg-white hover:text-[#ff4624] transition-colors select-none capitalize",
         {
-            "bg-green-500 pointer-events-none": status === a && verify === a,
-            "bg-green-500 pointer-events-none opacity-60": history && verify && pokemon.name === a && verify !== a,
-            "bg-red-700 pointer-events-none": status === 'failed' && verify && verify === a && pokemon.name !== a,
+            "bg-green-500 pointer-events-none focus:bg-green-500": status === a && verify === a,
+            "bg-green-500 pointer-events-none opacity-60 focus:bg-green-500": history && verify && pokemon.name === a && verify !== a,
+            "bg-red-700 pointer-events-none focus:bg-red-700": status === 'failed' && verify && verify === a && pokemon.name !== a,
             "pointer-events-none opacity-60 bg-transparent": verify && pokemon.name !== a && verify !== a
         }
     )
